62598d3a2fcfc84b9d9c9418d85a38a125c71d5e
Ruben <Ruben@DESKTOP-2GR0051>
2018-02-08 12:17:10 -0500

    test

---
+++ MyRepoDomain/Gral/PersonE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRepoDomain
+{
+    public sealed class PersonE
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MyRepoDomain/ViewModels/ResponseBasicVm.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRepoDomain
+{
+    class ResponseBasicVm
+    {
+    }
+}
