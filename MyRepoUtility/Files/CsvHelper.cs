480c218c938b94dcc6e790daa108ecacb43f870c
Ruben <Ruben@DESKTOP-2GR0051>
2018-02-09 16:44:46 -0500

    Base Core

--- MyRepoUtility/Files/CsvHelper.cs
+++ MyRepoUtility/Files/CsvHelper.cs
@@ -7,8 +7,17 @@ using System.Text;
 
 namespace MyRepoUtility
 {
+
     public class CsvHelper
     {
+        //The library can also write
+
+        /// <summary>
+        /// Converts raw csv to List<typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="rawString"></param>
+        /// <returns></returns>
         public List<T> ReadCsv<T>(string rawString)
         {
             using (TextReader tr = new StringReader(rawString))
