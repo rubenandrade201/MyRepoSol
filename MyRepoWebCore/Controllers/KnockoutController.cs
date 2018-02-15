9a2a0975d09a0a4d3113702094546e1c1b19ed65
Redesis Redesis <Redesis2018@outlook.com>
2018-02-15 10:27:23 -0500

    basics knocout done :)

--- MyRepoWebCore/Controllers/KnockoutController.cs
+++ MyRepoWebCore/Controllers/KnockoutController.cs
@@ -8,9 +8,36 @@ namespace MyRepoWebCore.Controllers
 {
     public class KnockoutController : Controller
     {
+        public static List<string> SampleData = new List<string>()
+        {
+            "Uno", "Dos","Tres"
+        };
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult LoopSample()
+        {
+            return View();
+        }
+
+        public IActionResult DataSample()
+        {
+            return View();
+        }
+
+        public IActionResult GetData()
+        {
+            return Json(SampleData);
+        }
+        
+        [HttpPost]
+        public IActionResult PostData(string value)
+        {
+            SampleData.Add(value);
+            return Json(true);
         }
     }
 }
